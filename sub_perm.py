7ccb0009c783384d3c86f5195eca586ff1d2b7a5
Şeyma Atmaca <54944249+SeymaAtmaca@users.noreply.github.com>
2023-01-16 21:58:07 +0300

    Add files via upload

---
+++ sub_perm.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def checkInclusion(self, s1, s2):
+        """
+        :type s1: str
+        :type s2: str
+        :rtype: bool
+        """
+
+    def checkInclusion(self, s1, s2):
+        cntr, w, match = Counter(s1), len(s1), 0     
+
+        for i in range(len(s2)):
+            if s2[i] in cntr:
+                if not cntr[s2[i]]: match -= 1
+                cntr[s2[i]] -= 1
+                if not cntr[s2[i]]: match += 1
+
+            if i >= w and s2[i-w] in cntr:
+                if not cntr[s2[i-w]]: match -= 1
+                cntr[s2[i-w]] += 1
+                if not cntr[s2[i-w]]: match += 1
+
+            if match == len(cntr):
+                return True
+
+        return False
+                    
+
+solution = Solution()
+s1 = "aa"
+s2 = "ba"
+result = solution.checkInclusion(s1, s2)
+print(result)
