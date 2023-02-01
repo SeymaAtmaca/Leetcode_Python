4310f356fa98499ec8aee996e375432408d4b012
Şeyma Atmaca <54944249+SeymaAtmaca@users.noreply.github.com>
2023-02-01 19:17:05 +0300

    Add files via upload

---
+++ longest_palindrom_with_counters.py
@@ -0,0 +1,16 @@
+from collections import Counter
+
+class Solution:
+    def longestPalindrome(self, words):
+
+        counter = Counter(words)
+        res = mid = 0 
+
+        for word in counter.keys():
+            if word[0]==word[1]:
+                res += counter[word] if counter[word]%2==0 else counter[word]-1
+                mid |= counter[word]%2
+            
+            elif word[::-1] in counter:
+                res += min(counter[word],counter[word[::-1]])
+        return (res + mid) * 2
