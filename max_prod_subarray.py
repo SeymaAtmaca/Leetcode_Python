60bbce7dd13acedfb6808db710753a351e71d8d4
Şeyma Atmaca <54944249+SeymaAtmaca@users.noreply.github.com>
2023-02-20 17:14:44 +0300

    Add files via upload

---
+++ max_prod_subarray.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def maxProduct(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        max_prod = [0]*len(nums)
+        min_prod = [0]*len(nums)
+        max_prod[0] = nums[0]
+        min_prod[0] = nums[0]
+        result = nums[0]
+
+        for i in range(1, len(nums)):
+            if nums[i] >= 0:
+                max_prod[i] = max(nums[i], max_prod[i-1]*nums[i])
+                min_prod[i] = min(nums[i], min_prod[i-1]*nums[i])
+
+            else : 
+                max_prod[i] = max(nums[i], min_prod[i-1] * nums[i])
+                min_prod[i] = min(nums[i], max_prod[i-1] * nums[i]) 
+            
+            result = max(result, max_prod[i])
+
+        return result
