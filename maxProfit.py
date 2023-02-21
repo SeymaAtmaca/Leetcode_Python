ae53d2994bed6cb69fe19eaf30db819483a82842
Şeyma Atmaca <54944249+SeymaAtmaca@users.noreply.github.com>
2023-02-21 18:59:48 +0300

    Add files via upload

---
+++ maxProfit.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        min_price = max(prices)
+        max_profit = 0
+
+        for price in prices:
+            if price < min_price :
+                min_price = price
+            if price - min_price > max_profit :
+                max_profit = price - min_price
+
+        return max_profit
